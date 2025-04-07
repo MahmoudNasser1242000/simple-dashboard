7783054b7e3af341a796253634e450bed0870218
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2025-04-07 02:32:00 +0200

    add header with logo

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+const Header = () => {
+    return (
+        <header className="w-full px-4 py-4 flex justify-between items-center border-b shadow-sm dark:border-gray-700">
+            <div className="flex items-center gap-3 text-xl font-bold text-gray-900 dark:text-white">
+                <Image
+                    src={"/images/logo.svg"}
+                    alt="logo"
+                    width={37}
+                    height={37}
+                />
+                <span className="font-mono">My Dashbord</span>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
